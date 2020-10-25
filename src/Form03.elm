b5e6edcf292ff65f540017953b2c5f5b2f45b6ea
Haruka Kai <plalal@gmail.com>
2020-10-25 12:00:15 +0900

    validator

--- src/Form03.elm
+++ src/Form03.elm
@@ -1,9 +1,13 @@
+module Form03 exposing (main)
+
 import Browser
 -- import Html exposing (..)
 import Html exposing (Html, div, input, text)
 -- import Html.Attributes exposing (..)
 import Html.Attributes exposing (type_, placeholder, value, style)
 import Html.Events exposing (onInput)
+import String exposing (length)
+import Char exposing (isUpper, isLower, isDigit)
 
 
 
@@ -20,6 +24,7 @@ main =
 
 type alias Model =
   { name : String
+  , age : String
   , password : String
   , passwordAgain : String
   }
@@ -27,7 +32,7 @@ type alias Model =
 
 init : Model
 init =
-  Model "" "" ""
+  Model "" "" "" ""
 
 
 
@@ -36,6 +41,7 @@ init =
 
 type Msg
   = Name String
+  | Age String
   | Password String
   | PasswordAgain String
 
@@ -45,6 +51,9 @@ update msg model =
   case msg of
     Name name ->
       { model | name = name }
+
+    Age age ->
+      { model | age = age }
 
     Password password ->
       { model | password = password }
@@ -61,6 +70,7 @@ view : Model -> Html Msg
 view model =
   div []
     [ viewInput "text" "Name" model.name Name
+    , viewInput "age" "Age" model.age Age
     , viewInput "password" "Password" model.password Password
     , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
     , viewValidation model
@@ -74,7 +84,31 @@ viewInput t p v toMsg =
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
+  if model.name == "" then
+    div [ style "color" "red" ] [ text "Name empty!" ]
   else
+  if model.age == "" then
+    div [ style "color" "red" ] [ text "Age empty!" ]
+  else
+  if String.toInt(model.age) == Nothing then
+    div [ style "color" "red" ] [ text "Age malformed!" ]
+  else
+  if model.password == "" then
+    div [ style "color" "red" ] [ text "Password empty!" ]
+  else
+  if model.password /= model.passwordAgain then
     div [ style "color" "red" ] [ text "Passwords do not match!" ]
+  else
+  if length model.password < 8 then
+    div [ style "color" "#880" ] [ text "Password too short." ]
+  else
+  if not (String.any isUpper model.password) then
+    div [ style "color" "#880" ] [ text "Password missing upper-case chars." ]
+  else
+  if not (String.any isLower model.password) then
+    div [ style "color" "#880" ] [ text "Password missing lower-case chars." ]
+  else
+  if not (String.any isDigit model.password) then
+    div [ style "color" "#880" ] [ text "Password missing digit chars." ]
+  else
+    div [ style "color" "green" ] [ text "OK" ]
